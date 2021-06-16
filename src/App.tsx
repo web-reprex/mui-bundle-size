e790c075227954d339e646f02ef7e0d25a0a8edf
Seyyed Mahdi Hassanpour <seyyedkhandon@gmail.com>
2021-06-16 12:58:58 +0430

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, TextField } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
+      <Button>this is mui button</Button>
+      <TextField value="this is text field" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
